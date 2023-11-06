9ba118fd09a2f75c59e62efd85360513466084c1
SWS-5007 <119567356+SWS-5007@users.noreply.github.com>
2023-11-06 02:36:46 -0500

    Added the template and changed the settings

--- members/views.py
+++ members/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def members(request):
-    return HttpResponse("Hello world!")
+    template = loader.get_template('myfirst.html')
+    return HttpResponse(template.render())
